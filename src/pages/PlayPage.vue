b0e5f35cd423eadf1a332b8ed162d8f6bcbf78aa
horitomoyuki <eziltom0131@gmail.com>
2022-01-16 11:41:57 +0900

    SCSSを使ってマス目を塗りつぶす

--- src/pages/PlayPage.vue
+++ src/pages/PlayPage.vue
@@ -14,6 +14,30 @@
   field[1][0] = 1;
   field[2][0] = 1;
   field[3][0] = 1;
+
+  const classBlockColor = (x: number, y: number): string => {
+    const type = field[y][x];
+    if (type > 0) {
+      switch (type) {
+        case 1:
+          return "block-i";
+        case 2:
+          return "block-o";
+        case 3:
+          return "block-s";
+        case 4:
+          return "block-z";
+        case 5:
+          return "block-j";
+        case 6:
+          return "block-l";
+        case 7:
+          return "block-t";
+        default:
+          return "";
+      }
+    }
+  }
 </script>
 
 <template>
@@ -28,13 +52,14 @@
           class="block"
           v-for="(col, x) in row"
           :key="() => `${x}${y}`"
-        >{{ col }}</td>
+          :class="classBlockColor(x, y)"
+        />
       </tr>
     </table>
   </div>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
   .container {
     display: flex;
     justify-content: center;
@@ -50,5 +75,31 @@
     width: 1em;
     height: 1em;
     border: 0.1px solid #95a5a6;
+  }
+
+  /*
+    各テトリミノに対応した色を扱うクラス定義
+    .block-i, .block-o のようにクラスが定義される
+  */
+  &-i {
+    background: #3498db;
+  }
+  &-o {
+    background: #f1c40f;
+  }
+  &-t {
+    background: #9b59b6;
+  }
+  &-j {
+    background: #1e3799;
+  }
+  &-l {
+    background: #e67e22;
+  }
+  &-s {
+    background: #2ecc71;
+  }
+  &-z {
+    background: #e74c3c;
   }
 </style>
